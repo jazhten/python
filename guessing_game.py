50edf2a52e4364f60a3a683947033601b651f90d
jaz <jazhten@github.com>
2018-11-15 21:55:21 +0800

    -a

--- guessing_game.py
+++ guessing_game.py
@@ -3,14 +3,19 @@ import random
 top = int(input('Enter highest number limit thing : '))
 
 answer = random.randint(0,top)
-print(answer)
-
+#print(answer)
+count = 0
 flag = True
 while flag == True:
+    count +=1
     guess = int(input('Enter guess '))
     if(guess == answer):
-        print("Yay!")
-        flag = False
+        print("Yay! You took " + str(count) +" guesses")
+        if input('Type exit to quit game:') == 'exit':
+            flag = False
+        else:
+            count = 0
+            answer = random.randint(0,top)
     if(guess > answer):
         print("Too high!")
     if(guess < answer):
